11dc8efa8115ad917271f2d5360391dac2d3dbef
ianmuchyri <ianmuchiri8@gmail.com>
2026-05-22 11:19:29 +0300

    add basepath redirects
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
+const BASE_PATH = "/docs/magistrala";
+
 /** @type {import('next').NextConfig} */
 const config = {
   output: "export",
@@ -10,6 +12,10 @@ const config = {
   serverExternalPackages: ["@takumi-rs/image-response"],
   images: {
     unoptimized: true,
+  },
+  basePath: BASE_PATH,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: BASE_PATH,
   },
 };
 
