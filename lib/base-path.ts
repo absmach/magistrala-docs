dd8b89870367eae27aa895d0eae3227184b85d51
ianmuchyri <ianmuchiri8@gmail.com>
2026-05-25 12:59:57 +0300

    have basepath use env variables
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- lib/base-path.ts
+++ lib/base-path.ts
@@ -1,4 +1,5 @@
-export const BASE_PATH = "/docs/magistrala";
+export const BASE_PATH =
+  process.env.NEXT_PUBLIC_BASE_PATH || "/docs/magistrala";
 
 export function withBasePath(path = "") {
   if (!path || path === "/") return BASE_PATH;
